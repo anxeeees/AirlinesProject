2e141b7059a654eb25acb743d0ea528e504b8490
Ester Stankovska <stankovskaester@gmail.com>
2024-03-31 15:51:52 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/airlinesproject/FTextField.java
@@ -0,0 +1,75 @@
+package com.mycompany.airlinesproject;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.BorderFactory;
+import javax.swing.JTextField;
+
+
+public class FTextField extends JTextField {
+    private Color fillColor;
+    private Color lineColor;
+    private int strokeWidth;
+
+    // Updated constructor to set default values
+    public FTextField() {
+        fillColor = new Color(255, 255, 255);
+        lineColor = new Color(227, 227, 226); // Default line color
+        strokeWidth = 1;
+        setOpaque(false);
+        setBorder(BorderFactory.createEmptyBorder(2, 10, 2, 10));
+    }
+
+    // Additional constructor to set custom default line color
+    public FTextField(Color defaultLineColor) {
+        fillColor = new Color(255, 255, 255);
+        lineColor = defaultLineColor; // Set the custom default line color
+        strokeWidth = 1;
+        setOpaque(false);
+        setBorder(BorderFactory.createEmptyBorder(2, 10, 2, 10));
+    }
+
+    public Color getFillColor() {
+        return fillColor;
+    }
+
+    public void setFillColor(Color fillColor) {
+        this.fillColor = fillColor;
+    }
+
+    public Color getLineColor() {
+        return lineColor;
+    }
+
+    public void setLineColor(Color lineColor) {
+        this.lineColor = lineColor;
+    }
+
+    public int getStrokeWidth() {
+        return strokeWidth;
+    }
+
+    public void setStrokeWidth(int strokeWidth) {
+        this.strokeWidth = strokeWidth;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (!isOpaque()) {
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            int s = strokeWidth;
+            int w = getWidth() - (2 * s);
+            int h = getHeight() - (2 * s);
+            g2d.setColor(fillColor);
+            g2d.fillRoundRect(s, s, w, h, h, h);
+            g2d.setStroke(new BasicStroke(s));
+            g2d.setColor(lineColor);
+            g2d.drawRoundRect(s, s, w, h, h, h);
+        }
+        super.paintComponent(g);
+    }
+}
---
+++ src/main/java/com/mycompany/airlinesproject/tb_pass_name.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.airlinesproject;
+
+/**
+ *
+ * @author Ester
+ */
+class tb_pass_name {
+    
+}
